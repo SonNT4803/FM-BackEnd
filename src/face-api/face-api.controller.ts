51bd729d73b636a44a68869c4fa2d518f39a5bb5
Nguyễn Thái Sơn <sonnthe172793@fpt.edu.vn>
2025-06-27 03:25:20 +0700

    f

--- src/face-api/face-api.controller.ts
+++ src/face-api/face-api.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, UseGuards, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Get,
+  Param,
+  HttpStatus,
+} from '@nestjs/common';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { FaceApiService } from './face-api.service';
 
@@ -9,12 +17,24 @@ export class FaceApiController {
 
   @Get('test-avatar/:studentId')
   async testAvatarFormat(@Param('studentId') studentId: number) {
-    return this.faceApiService.testAvatarFormat(studentId);
+    const result = await this.faceApiService.testAvatarFormat(studentId);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Kiểm tra format ảnh avatar thành công',
+      data: result,
+    };
   }
 
   @Get('test-face-detection/:studentId')
   async testFaceDetection(@Param('studentId') studentId: number) {
-    return this.faceApiService.testFaceDetection(studentId);
+    const result = await this.faceApiService.testFaceDetection(studentId);
+    return {
+      statusCode: HttpStatus.OK,
+      message: result.success
+        ? 'Test face detection thành công'
+        : 'Test face detection thất bại',
+      data: result,
+    };
   }
 
   @Post('verify-face')
@@ -28,13 +48,19 @@ export class FaceApiController {
       note?: string;
     },
   ) {
-    return await this.faceApiService.verifyFace(
+    const result = await this.faceApiService.verifyFace(
       body.image,
       body.studentId,
       body.scheduleId,
       body.note,
     );
+    return {
+      statusCode: HttpStatus.OK,
+      message: result.success ? 'Điểm danh thành công' : 'Điểm danh thất bại',
+      data: result,
+    };
   }
+
   @Post('verify-class')
   @ApiBearerAuth('JWT-auth')
   async verifyClass(
@@ -46,11 +72,16 @@ export class FaceApiController {
       scheduleId: number;
     },
   ) {
-    return this.faceApiService.verifyClass(
+    const result = await this.faceApiService.verifyClass(
       body.image,
       body.classId,
       body.teacherId,
       body.scheduleId,
     );
+    return {
+      statusCode: HttpStatus.OK,
+      message: `Xác thực thành công ${result.verified_count} sinh viên trong tổng số ${result.total_faces_detected} khuôn mặt được phát hiện`,
+      data: result,
+    };
   }
 }
